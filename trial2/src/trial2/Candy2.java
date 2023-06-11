bd91c7be643c4bd36ff79cbb3e24b6bb6baaa4ab
Ahmed Hossam <116103656+AhmedHossam200@users.noreply.github.com>
2023-06-11 20:50:11 +0300

    Add files via upload

---
+++ trial2/src/trial2/Candy2.java
@@ -0,0 +1,12 @@
+package trial2;
+
+
+
+public class Candy2 extends Candy{
+String icon;
+
+    public Candy2(){
+        super("Lozenge",2);
+      icon ="C:\\Users\\Android-161220\\Desktop\\CANDY java\\candies\\c222.png";
+    }
+}
